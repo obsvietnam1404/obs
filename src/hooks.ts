ac8a3283e4b1900e1cc43e886ae3e6307b43fc55
obsvietnam1404 <trungtin17.99@gmail.com>
2025-07-02 16:40:23 +0700

    Initialize fix/cleanup-warnings

---
+++ src/hooks.ts
@@ -0,0 +1,138 @@
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { MutableRefObject, useLayoutEffect, useMemo, useState } from "react";
+import toast from "react-hot-toast";
+import { UIMatch, useMatches, useNavigate } from "react-router-dom";
+import { cartState, cartTotalState } from "@/state";
+import { Product } from "@/types";
+import { getConfig } from "@/utils/template";
+import { createOrder, openChat, purchase } from "zmp-sdk";
+
+export function useRealHeight(
+  element: MutableRefObject<HTMLDivElement | null>,
+  defaultValue?: number
+) {
+  const [height, setHeight] = useState(defaultValue ?? 0);
+  useLayoutEffect(() => {
+    if (element.current && typeof ResizeObserver !== "undefined") {
+      const ro = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+        const [{ contentRect }] = entries;
+        setHeight(contentRect.height);
+      });
+      ro.observe(element.current);
+      return () => ro.disconnect();
+    }
+    return () => {};
+  }, [element.current]);
+
+  if (typeof ResizeObserver === "undefined") {
+    return -1;
+  }
+  return height;
+}
+
+export function useAddToCart(product: Product) {
+  const [cart, setCart] = useAtom(cartState);
+
+  const currentCartItem = useMemo(
+    () => cart.find((item) => item.product.id === product.id),
+    [cart, product.id]
+  );
+
+  const addToCart = (
+    quantity: number | ((oldQuantity: number) => number),
+    options?: { toast: boolean }
+  ) => {
+    setCart((cart) => {
+      const newQuantity =
+        typeof quantity === "function"
+          ? quantity(currentCartItem?.quantity ?? 0)
+          : quantity;
+      if (newQuantity <= 0) {
+        cart.splice(cart.indexOf(currentCartItem!), 1);
+      } else {
+        if (currentCartItem) {
+          currentCartItem.quantity = newQuantity;
+        } else {
+          cart.push({
+            product,
+            quantity: newQuantity,
+          });
+        }
+      }
+      return [...cart];
+    });
+    if (options?.toast) {
+      toast.success("Đã thêm vào giỏ hàng");
+    }
+  };
+
+  return { addToCart, cartQuantity: currentCartItem?.quantity ?? 0 };
+}
+
+export function useCustomerSupport() {
+  return () =>
+    openChat({
+      type: "oa",
+      id: getConfig((config) => config.template.oaIDtoOpenChat),
+    });
+}
+
+export function useToBeImplemented() {
+  return () =>
+    toast("Chức năng dành cho các bên tích hợp phát triển...", {
+      icon: "🛠️",
+    });
+}
+
+export function useCheckout() {
+  const { totalAmount } = useAtomValue(cartTotalState);
+  const [cart, setCart] = useAtom(cartState);
+  const navigate = useNavigate();
+
+  return async () => {
+    try {
+      await createOrder({
+        amount: totalAmount,
+        desc: "Thanh toán đơn hàng",
+        item: cart.map((item) => ({
+          id: item.product.id,
+          name: item.product.name,
+          price: item.product.price,
+          quantity: item.quantity,
+        })),
+      });
+      setCart([]);
+      navigate("/orders", {
+        viewTransition: true,
+      });
+      toast.success("Thanh toán thành công. Cảm ơn bạn đã mua hàng!", {
+        icon: "🎉",
+        duration: 5000,
+      });
+    } catch (error) {
+      console.warn(error);
+      toast.error(
+        "Thanh toán thất bại. Vui lòng kiểm tra nội dung lỗi bên trong Console."
+      );
+    }
+  };
+}
+
+export function useRouteHandle() {
+  const matches = useMatches() as UIMatch<
+    undefined,
+    | {
+        title?: string | Function;
+        logo?: boolean;
+        search?: boolean;
+        noFooter?: boolean;
+        noBack?: boolean;
+        noFloatingCart?: boolean;
+        scrollRestoration?: number;
+      }
+    | undefined
+  >[];
+  const lastMatch = matches[matches.length - 1];
+
+  return [lastMatch.handle, lastMatch, matches] as const;
+}
